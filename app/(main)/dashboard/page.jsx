2379eaea4501d6f6753bb5861b46d797a6843430
harshitbajp <harshit.bajpai.77777@gmail.com>
2024-12-17 01:24:25 +0530

    First commit (hero page, db setup, auth setup, account creation)

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,29 @@
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import React from "react";
+
+const DashboardPage = async () => {
+  return (
+    <div className="px-5">
+      {/* Budget Progress */}
+
+      {/* Dashboard Overview */}
+
+      {/* Account Grid */}
+
+      <div className="grip gap-4 md:grid-col-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ components/create-account-drawer.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React, { useState } from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+const CreateAccountDrawer = ({children}) => {
+
+    const [ open, setOpen] = useState(false)
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+       <div>
+        <form></form>
+       </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CreateAccountDrawer;
