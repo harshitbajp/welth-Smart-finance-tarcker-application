c0ead446efc70faefb95cd296a39e451104e2c84
harshitbajp <harshit.bajpai.77777@gmail.com>
2024-12-21 17:42:39 +0530

    Account-page and transaction-charts

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 import TransactionTable from "./_components/transaction-table";
 import { BarLoader } from "react-spinners";
+import AccountChart from "./_components/account-chart";
 
 const AccountsPage = async ({ params }) => {
   const { id } = await params;
@@ -41,6 +42,11 @@ const AccountsPage = async ({ params }) => {
       </div>
 
       {/* chart section */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions}/>
+      </Suspense>
 
       {/* transaction table */}
       <Suspense
